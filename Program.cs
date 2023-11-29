88a144528be5af9e95b847a82af8a06b5ca33257
Sagar Adhikari <sagarjungadhikari19@gmail.com>
2023-11-29 11:55:57 +0545

    array showed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Example{
+
+    public  static void showArray<T>(T[] arr) {
+
+        for   (int i=0;i<arr.Length;i++){
+            Console.WriteLine(arr[i]);
+        }
+
+    }
+}
+
+
+class Program{
+
+    static void Main(string[] args){
+
+        int[] Numbers =new int[4];
+        Numbers[0]=11;
+         Numbers[1]=31;
+        Numbers[2]=21;
+   
+      
+
+        string[] Names=new string[4];
+        Names[0]="sagar";
+        Names[1]="sagarika";
+        Names[2]="sagarmatha";
+        Names[3]="samundra";
+
+        Example.showArray(Numbers);
+         Example.showArray(Names);
+
+
+    }
+}
